ae717bff2851f2a09cd0b119ff313725a4bb4603
aadityakalwani <aadityakalwani@gmail.com>
2021-08-25 15:06:10 +0100

    basically finished

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 # ciao
 # may the coding begin
+
+import webbrowser
+import time
+
+
+def open_crypto():
+    webbrowser.open("https://aplha.multifarm.fi/assets")
+    time.sleep(0.25)
+    webbrowser.open("https://pancakeswap.finance/")
+    time.sleep(0.25)
+    webbrowser.open("https://apeswap.finance/")
+    time.sleep(0.25)
+    webbrowser.open("https://app.barbequeswap.finance/")
+    time.sleep(0.25)
+    webbrowser.open("https://bear.honeyfarm.finance/")
+    time.sleep(0.25)
+    webbrowser.open("https://pancakeswap.finance/")
+    time.sleep(0.25)
+
+
+open_crypto()
