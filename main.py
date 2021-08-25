18b86532bfa2ede966aa4aa140fac31084bf4867
aadityakalwani <aadityakalwani@gmail.com>
2021-08-25 15:28:35 +0100

    minor changes again, mainly slowing it down

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import time
 def open_crypto():
     time.sleep(0.5)
     webbrowser.open("https://aplha.multifarm.fi/assets")
-    time.sleep(0.25)
+    time.sleep(0.5)
     webbrowser.open("https://pancakeswap.finance/")
-    time.sleep(0.25)
+    time.sleep(0.5)
     webbrowser.open("https://apeswap.finance/")
-    time.sleep(0.25)
-    webbrowser.open("https://app.barbequeswap.finance/")
-    time.sleep(0.25)
+    time.sleep(0.5)
+    webbrowser.open("https://app.barbecueswap.finance/")
+    time.sleep(0.5)
     webbrowser.open("https://bear.honeyfarm.finance/")
-    time.sleep(0.25)
+    time.sleep(0.5)
 
 
 open_crypto()
