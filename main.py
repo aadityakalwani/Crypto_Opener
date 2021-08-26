8cd32baff9770fe9e11ed22050fded838681a787
aadityakalwani <aadityakalwani@gmail.com>
2021-08-26 09:22:16 +0100

    added another website, tenguswap

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ import time
 
 def open_crypto():
     time.sleep(0.5)
-    webbrowser.open("https://aplha.multifarm.fi/assets")
-    time.sleep(0.25)
+    webbrowser.open("https://alpha.multifarm.fi/assets")
+    time.sleep(0.5)
     webbrowser.open("https://pancakeswap.finance/")
-    time.sleep(0.25)
+    time.sleep(0.5)
     webbrowser.open("https://apeswap.finance/")
-    time.sleep(0.25)
-    webbrowser.open("https://app.barbequeswap.finance/")
-    time.sleep(0.25)
+    time.sleep(0.5)
+    webbrowser.open("https://app.barbecueswap.finance/")
+    time.sleep(0.5)
     webbrowser.open("https://bear.honeyfarm.finance/")
-    time.sleep(0.25)
+    time.sleep(0.5)
+    webbrowser.open("https://bsc.tenguswap.com/")
 
 
 open_crypto()
