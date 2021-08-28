842c361ba28caf0a5ed516dfcdd9f248bdbcd8e7
aadityakalwani <aadityakalwani@gmail.com>
2021-08-28 23:33:47 +0100

    added a new website and also made it so that it will open in a new window

--- main.py
+++ main.py
@@ -6,21 +6,18 @@ import time
 
 
 def open_crypto():
-    print("begun")
     time.sleep(0.5)
     webbrowser.open("https://alpha.multifarm.fi/assets")
-    time.sleep(0.25)
+    time.sleep(0.5)
     webbrowser.open("https://pancakeswap.finance/")
-    time.sleep(0.25)
+    time.sleep(0.5)
     webbrowser.open("https://apeswap.finance/")
-    time.sleep(0.25)
-    webbrowser.open("https://app.barbequeswap.finance/")
-    time.sleep(0.25)
+    time.sleep(0.5)
+    webbrowser.open("https://app.barbecueswap.finance/")
+    time.sleep(0.5)
     webbrowser.open("https://bear.honeyfarm.finance/")
-    time.sleep(0.25)
-    webbrowser.open("https://lp-swap.io/")
-    time.sleep(0.25)
-    webbrowser.open("https://cobra.exchange/#/staking/pools")
+    time.sleep(0.5)
+    webbrowser.open("https://cobraswap.finance/")
 
 
 open_crypto()
