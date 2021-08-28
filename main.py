e110d3f9483840cfa13ba1c8c3feb9bfc5f3a55a
aadityakalwani <aadityakalwani@gmail.com>
2021-08-28 23:46:34 +0100

    amazing, used spotlight/raycast via pyautogui to open chrome, then cmd+n to open a new window, then it opens the tabs

--- main.py
+++ main.py
@@ -3,21 +3,40 @@
 
 import webbrowser
 import time
+import pyautogui
 
 
 def open_crypto():
-    time.sleep(0.5)
+    pyautogui.keyDown("command")
+    pyautogui.keyDown("space")
+    time.sleep(0.25)
+    pyautogui.keyUp("space")
+    pyautogui.keyUp("command")
+    time.sleep(0.25)
+    pyautogui.write("chrome", interval=0.25)
+    time.sleep(0.25)
+    pyautogui.press("enter")
+    time.sleep(1)
+    pyautogui.keyDown("command")
+    pyautogui.keyDown("n")
+    time.sleep(0.25)
+    pyautogui.keyUp("n")
+    pyautogui.keyUp("command")
+    time.sleep(1)
+
     webbrowser.open_new("https://alpha.multifarm.fi/assets")
     time.sleep(0.5)
-    webbrowser.open("https://pancakeswap.finance/")
+    webbrowser.open_new("https://dailydefi.org/tools/impermanent-loss-calculator/")
     time.sleep(0.5)
-    webbrowser.open("https://apeswap.finance/")
+    webbrowser.open_new("https://pancakeswap.finance/")
     time.sleep(0.5)
-    webbrowser.open("https://app.barbecueswap.finance/")
+    webbrowser.open_new("https://apeswap.finance/")
     time.sleep(0.5)
-    webbrowser.open("https://bear.honeyfarm.finance/")
+    webbrowser.open_new("https://app.barbecueswap.finance/")
     time.sleep(0.5)
-    webbrowser.open("https://cobraswap.finance/")
+    webbrowser.open_new("https://bear.honeyfarm.finance/")
+    time.sleep(0.5)
+    webbrowser.open_new("https://cobraswap.finance/")
 
     print("done")
 
